288869f28449727e3adbdaaa95948646e76e0271
Michael Ong <cork.hack12@gmail.com>
2024-05-09 00:14:07 +0800

    Added threlte

---
+++ src/lib/components/Scene.svelte
@@ -0,0 +1,25 @@
+<script>
+  import { T } from '@threlte/core'
+  import { Float,OrbitControls,Grid } from '@threlte/extras'
+  import Star from './Star.svelte'
+</script>
+
+
+<T.PerspectiveCamera makeDefault position={[-5, 6, 10]} fov={25}>
+	<OrbitControls enableDamping target={[0, 0, 0]} />
+</T.PerspectiveCamera>
+
+<T.DirectionalLight intensity={1.8} position={[0, 10, 0]} castShadow shadow.bias={-0.0001} />
+<T.AmbientLight intensity={0.2} />
+
+<Grid
+	position.y={-0.001}
+	cellColor="#ffffff"
+	sectionColor="#ffffff"
+	sectionThickness={0}
+	fadeDistance={25}
+	cellSize={2}
+/>
+
+
+<Star/>
---
+++ src/lib/components/Star.svelte
@@ -0,0 +1,28 @@
+<script>
+    import { T } from '@threlte/core'
+    import { TextureLoader } from 'three'
+    import { useLoader } from '@threlte/core'
+    import { Float } from '@threlte/extras'
+
+    const textures = useLoader(TextureLoader).load({
+        map: 'src/lib/textures/star.jpg',
+        emissiveMap: 'src/lib/textures/star.jpg',
+        alphaMap: 'src/lib/textures/star_alpha.jpg'
+    })
+</script>
+  
+<Float
+floatIntensity={1}
+floatingRange={[0, 1]}
+>
+<T.Mesh
+    position.y={1.2}
+    position.z={-0.75}
+>
+    <T.PlaneGeometry />
+    {#await textures then values}
+    <T.MeshStandardMaterial {...values} />
+    {/await}
+</T.Mesh>
+</Float>
+  
